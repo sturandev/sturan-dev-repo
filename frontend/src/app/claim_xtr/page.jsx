47e9f85642eb1003c17762a800f6ce807fb02ad4
cingihimut <cingihimutbanget@gmail.com>
2024-05-29 21:31:53 +0700

    adding give a disclaimer to the user

--- frontend/src/app/claim_xtr/page.jsx
+++ frontend/src/app/claim_xtr/page.jsx
@@ -2,6 +2,7 @@
 import Image from "next/image"
 import { useState } from "react"
 import { claimToken } from "../utils/claimToken";
+import Link from "next/link";
 
 const Page = () => {
   const [isClaimed, setIsClaimed] = useState(false);
@@ -22,7 +23,7 @@ const Page = () => {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-color-primary bg-opacity-10">
-      <div className="border-2 border-color-gray border-opacity-15 rounded-xl p-24 custom-shadow">
+      <div className="border-2 border-color-gray border-opacity-15 rounded-xl p-10 custom-shadow flex flex-col items-center">
         <div className="flex items-center justify-center w-40 h-40">
           {isClaimed ? (
             <Image src="/assets/kado-terbuka.png" height="100" width="100" alt="kado" />
@@ -30,10 +31,11 @@ const Page = () => {
             <Image src="/assets/kado-tertutup.png" height="100" width="100" alt="kado" />
           )}
         </div>
-        <div className="mt-4 flex justify-center">
-          <button onClick={handleClick} disabled={isLoading} className="p-2 bg-color-primary rounded-xl hover:bg-opacity-70 transition duration-300">
+        <div className="mt-4 flex flex-col items-center">
+          <button onClick={handleClick} disabled={isLoading} className="p-2 bg-color-primary rounded-xl hover:bg-opacity-70 transition duration-300 mb-4">
             {isLoading ? "Loading..." : "Claim XTR"}
           </button>
+          <p className="text-center">If you don't have MetaMask, you can download it from this <Link href="https://chromewebstore.google.com/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn?hl=id" className="text-blue-500 hover:underline">Link</Link></p>
         </div>
       </div>
     </div>
