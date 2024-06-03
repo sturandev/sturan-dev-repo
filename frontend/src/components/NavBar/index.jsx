0510f2a4c9f244e61c29c47ec6176e7a0546fd21
cingihimut <cingihimutbanget@gmail.com>
2024-06-03 17:49:44 +0700

    fix: Error: No Next.js version could be detected in your project.

--- frontend/src/components/NavBar/index.jsx
+++ frontend/src/components/NavBar/index.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
             <header className="bg-color-primary bg-opacity-10 border-b-2 border-color-gray border-opacity-10 p-4 sm:p-6">
                 <div className="flex justify-between items-center ml-4 mr-4 sm:ml-14 sm:mr-16">
                     <Link href="/" className="font-bold text-lg sm:text-xl text-color-primary">
-                        Crowdfunding
+                        Sturan Network
                     </Link>
                     <ConnectButton />
                 </div>
