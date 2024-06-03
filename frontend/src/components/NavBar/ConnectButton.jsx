b4cce85bd9c964568ee8af163bfc44b9c78ff059
cingihimut <cingihimutbanget@gmail.com>
2024-06-03 17:57:19 +0700

    fix conflict

--- frontend/src/components/NavBar/ConnectButton.jsx
+++ frontend/src/components/NavBar/ConnectButton.jsx
@@ -1,22 +1,6 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-"use client"
-import { connectWeb3 } from "@/app/utils/web3";
-import { useState } from "react";
-=======
-"use client";
-import React, { useState } from 'react';
-import { config, projectId } from '../../app/config';
-import { createWeb3Modal } from '@web3modal/wagmi/react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { WagmiProvider } from 'wagmi';
-import { connectWeb3 } from '../../app/utils/web3';
->>>>>>> 9d044a39d50702357625529d6d4b3c812c1bc1b2
-=======
 "use client"
 import { useState } from "react";
 import { connectWeb3 } from "../../app/utils/web3";
->>>>>>> e040c0b79a24a13e857f2a7f0f2febf7ddccc5d0
 
 const ConnectButton = () => {
 
